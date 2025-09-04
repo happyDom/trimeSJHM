5e02de35e58f5530e8b879ef7a65251611395581
dyyDefault <douyaoyuan@126.com>
2025-09-04 16:30:49 +0800

    feat：更新lua脚本 inputShow_Filter.lua

--- lua/inputShow_Filter.lua
+++ lua/inputShow_Filter.lua
@@ -12,43 +12,28 @@ if logEnable then
 end
 
 local candsCnt = 0
-local candsIdx = -1
 local cands = {}
+local allCands = {}
 
 local function _inputShow(input, env)
 	local inputStr = tostring(env.engine.context.input)
 	local inputStrLen = string.len(inputStr)
 	
 	candsCnt = 0
-	candsIdx = -1
 	cands = {}
+	allCands = {}
 	for cand in input:iter() do
-		candsIdx = candsIdx + 1
-		
+		table.insert(allCands, cand)
 		if inputStrLen > 3 or '/' == inputStr:sub(1,1) then
 			-- 3码以上，或者以 / 引导的输入，不做处理
 			yield(cand)
 		elseif utf8String.utf8Len(cand.text) < 2 then
 			-- 3码以内，只出字
-			if '*' == inputStr:sub(1,1) then
-			-- 如果以 * 开头，则1码只出 5 个字，2码只出 20 个字
-				yield(cand)
-				candsCnt = candsCnt + 1
-				if 1 == inputStrLen then
-					if candsCnt >= 5 then
-						break
-					end
-				elseif 2 == inputStrLen then
-					if candsCnt >= 20 then
-						break
-					end
-				end
-			elseif inputStrLen < 3 then
-				-- 1码只出一简字，或二简字
+			if inputStrLen < 3 then
+				-- 3码以下，只出一简字，或二简字
 				if '~' == cand.comment:sub(1,1) then
 					break
 				else
-					-- yield(cand)
 					table.insert(cands, cand)
 					candsCnt = candsCnt + 1
 				end
@@ -70,6 +55,13 @@ local function _inputShow(input, env)
 		end)
 		for idx = 1, #cands do
 			yield(cands[idx])
+		end
+	end
+	
+	if inputStrLen <= 3 and candsCnt < 1 then
+		-- 如果3码及以下，但没有候选项出来，则抛出所有候选项
+		for idx = 1, #allCands do
+			yield(allCands[idx])
 		end
 	end
 end
